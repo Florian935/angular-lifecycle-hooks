233c81f6863eb07bf521cb3dbfd5ae2e2133dc0c
florian935 <florian.martin63000@gmail.com>
2021-09-30 10:24:37 +0200

    Feat: added demo of OnChanges hooks

--- src/app/pages/on-changes/on-changes-child/on-changes-child.component.ts
+++ src/app/pages/on-changes/on-changes-child/on-changes-child.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+} from '@angular/core';
+import { Customer } from 'src/app/shared/models/customer';
 
 @Component({
-  selector: 'app-on-changes-child',
-  templateUrl: './on-changes-child.component.html',
-  styleUrls: ['./on-changes-child.component.scss']
+    selector: 'app-on-changes-child',
+    templateUrl: './on-changes-child.component.html',
+    styleUrls: ['./on-changes-child.component.scss'],
 })
-export class OnChangesChildComponent implements OnInit {
+export class OnChangesChildComponent implements OnChanges {
+    @Input() customer?: Customer;
+    changeLog: Array<string> = [];
 
-  constructor() { }
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('OnChanges');
+        console.log(JSON.stringify(changes));
 
-  ngOnInit(): void {
-  }
-
+        for (const propName in changes) {
+            const change = changes[propName];
+            const to = JSON.stringify(change.currentValue);
+            const from = JSON.stringify(change.previousValue);
+            const changeLog = `${propName}: changed from ${from} to ${to} `;
+            this.changeLog.push(changeLog);
+        }
+    }
 }
--- src/app/pages/on-changes/on-changes.component.ts
+++ src/app/pages/on-changes/on-changes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/shared/models/customer';
 
 @Component({
-  selector: 'app-on-changes',
-  templateUrl: './on-changes.component.html',
-  styleUrls: ['./on-changes.component.scss']
+    selector: 'app-on-changes',
+    templateUrl: './on-changes.component.html',
+    styleUrls: ['./on-changes.component.scss'],
 })
-export class OnChangesComponent implements OnInit {
+export class OnChangesComponent {
+    customer?: Customer;
 
-  constructor() { }
+    onClick(id: string | number, name: string): void {
+        if (!(id && name)) {
+            return;
+        }
 
-  ngOnInit(): void {
-  }
-
+        this.customer = { id, name };
+    }
 }
